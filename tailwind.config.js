71f00789f8783f424f1ff8b80398e50611b9a8b2
Martin M. <34661724+martin640@users.noreply.github.com>
2025-09-30 20:21:11 +0200

    initial website design

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	prefix: 'tw-',
+	content: ["./src/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				'brand-linkedin': '#0a66c2',
+				'brand-php': '#474A8A',
+				'brand-mysql': '#00758F',
+				'brand-redis': '#A41E11',
+				'brand-rust': '#1E2650',
+				'brand-minio': '#C51B3F',
+				'brand-typescript': '#3178C6',
+				'brand-electron': '#272A37',
+				'brand-react': '#61DBFB',
+				'brand-tailwind': '#00BCFF',
+				'link': '#00A6F4'
+			},
+			fontSize: {
+				'2xs': '10px'
+			}
+		},
+	},
+	plugins: [],
+}
+
